2c42b27a9e9f0bd4dd21a0beeb2ff194525f8dd8
Roman Prokopenko <prokopenkor5@gmail.com>
2016-11-16 13:28:07 +0200

    equipment and its materials defined

---
+++ src/main/java/ua/training/model/entity/Equipment.java
@@ -0,0 +1,52 @@
+package ua.training.model.entity;
+
+/**
+ * Created by Graffit on 16.11.2016.
+ */
+public abstract class Equipment {
+
+    /**
+     * equipment price
+     */
+    private double price;
+
+    /**
+     * equipment weight
+     */
+    private double weight;
+
+    /**
+     * material of equipment
+     */
+    private Material material;
+
+    public Equipment(double price, double weight, Material material) {
+        this.price = price;
+        this.weight = weight;
+        this.material = material;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+}
---
+++ src/main/java/ua/training/model/entity/Material.java
@@ -0,0 +1,8 @@
+package ua.training.model.entity;
+
+/**
+ * Created by Graffit on 16.11.2016.
+ */
+public enum Material {
+    LEATHER, IRON, STEEL
+}

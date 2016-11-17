87480d42f0da222a7910cc19c4aec2f43c5c420d
Roman Prokopenko <prokopenkor5@gmail.com>
2016-11-17 18:36:16 +0200

    torso with types in new folder

---
+++ src/main/java/ua/training/model/entity/equipment/torso/TorsoType.java
@@ -0,0 +1,10 @@
+package ua.training.model.entity.equipment.torso;
+
+/**
+ * Types of leg equipment
+ *
+ * @author Roman Prokopenko
+ */
+public enum TorsoType {
+    BRIGANDINE, HAUBERK, CUIRASS, PLACKART
+}

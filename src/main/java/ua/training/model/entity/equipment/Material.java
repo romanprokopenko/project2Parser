234a3b588aebceb1bcac6958d53295f6f41a29d9
Roman Prokopenko <prokopenkor5@gmail.com>
2016-11-17 18:41:43 +0200

    fixed formatting

--- src/main/java/ua/training/model/entity/equipment/Material.java
+++ src/main/java/ua/training/model/entity/equipment/Material.java
@@ -1,7 +1,7 @@
 package ua.training.model.entity.equipment;
 
 /**
- * Created by Graffit on 16.11.2016.
+ * Created by Roman Prokopenko on 16.11.2016.
  */
 public enum Material {
     LEATHER, IRON, STEEL

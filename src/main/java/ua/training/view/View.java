a774f5afa1ea9016c149e2a0384d1ba09713baf4
Roman Prokopenko <prokopenkor5@gmail.com>
2016-12-06 21:05:34 +0200

    view interface

--- src/main/java/ua/training/view/View.java
+++ src/main/java/ua/training/view/View.java
@@ -1,27 +1,10 @@
 package ua.training.view;
 
 /**
- * Class which used to interact with player
- *
- * @author Roman Prokopenko
+ * Created by Graffit on 06.12.2016.
  */
-public class View {
+public interface View {
+    void printMessage(String message);
 
-    public static final String UNSORTED_MESSAGE = "unsorted list of equipment:";
-    public static final String SORTED_MESSAGE = "sorted by weight list of equipment:";
-    public static final String PRICE_MESSAGE = "total price of equipment:";
-    public static final String FIND_MESSAGE = "list of equipment of specified price:";
-
-    /**
-     * Prints message to System.out
-     *
-     * @param message any string
-     */
-    public void printMessage(String message) {
-        System.out.println(message);
-    }
-
-    public void printNumberFormatted(Number message) {
-        System.out.println(String.format("%1.2f", message));
-    }
+    void printNumberFormatted(Number message);
 }

21b3e6925aa7d7ad30c8f39a14a3618a4ca1b182
Roman Prokopenko <prokopenkor5@gmail.com>
2016-12-06 21:05:50 +0200

    main

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -1,7 +1,10 @@
 package ua.training;
 
 import ua.training.control.Control;
+import ua.training.control.ParserControl;
 import ua.training.model.Model;
+import ua.training.model.ParserModel;
+import ua.training.view.ParserView;
 import ua.training.view.View;
 
 /**
@@ -13,9 +16,9 @@ public class Main {
     public static void main(String[] args) {
 
         //initialization of components
-        View view = new View();
-        Model model = new Model();
-        Control control = new Control(model, view);
+        View view = new ParserView();
+        Model model = new ParserModel();
+        Control control = new ParserControl(model, view);
 
         //run
         control.processUser();

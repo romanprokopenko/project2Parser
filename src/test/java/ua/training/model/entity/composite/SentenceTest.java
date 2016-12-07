5635deeeb9519e543625be93149ade5ce889f4d4
romanprokopenko <prokopenkor5@gmail.com>
2016-12-07 11:47:31 +0200

    test updated

--- src/test/java/ua/training/model/entity/composite/SentenceTest.java
+++ src/test/java/ua/training/model/entity/composite/SentenceTest.java
@@ -10,15 +10,16 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 /**
- * Created by Graffit on 06.12.2016.
+ * Created by Roman Prokopenko on 06.12.2016.
  */
 public class SentenceTest {
 
+    private Sentence sentence;
 
-    Sentence sentence;
     @Before
     public void init() {
-        String testString = "Во время наследования возможно изменения модификаторов доступа в сторону большей видимости;.";
+        String testString = "Во время наследования возможно изменения" +
+                " модификаторов доступа в сторону большей видимости;.";
         sentence = new Sentence();
         sentence.parse(testString);
     }
@@ -33,13 +34,14 @@ public class SentenceTest {
     @Test
     public void findLastWordIndex() throws Exception {
         int actualIndex = sentence.findLastWordIndex(sentence.subElements);
-        int expectedIndex = sentence.subElements.size()-3;
+        int expectedIndex = sentence.subElements.size() - 3;
         Assert.assertEquals(expectedIndex, actualIndex);
     }
 
     @Test
     public void swapListElements() throws Exception {
-        String testString = "видимости время наследования возможно изменения модификаторов доступа в сторону большей Во;.";
+        String testString = "видимости время наследования возможно" +
+                " изменения модификаторов доступа в сторону большей Во;.";
         Sentence tmpSentence = new Sentence();
         tmpSentence.parse(testString);
         List<ElementOfText> expectedList = tmpSentence.subElements;

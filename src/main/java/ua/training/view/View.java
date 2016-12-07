e34cbcd01c43c957c4bcfdbc07589aba918a1b63
romanprokopenko <prokopenkor5@gmail.com>
2016-12-07 11:47:54 +0200

    constants moved to interface

--- src/main/java/ua/training/view/View.java
+++ src/main/java/ua/training/view/View.java
@@ -7,12 +7,13 @@ package ua.training.view;
  */
 public interface View {
 
+    String START_MESSAGE = "text is read from file:";
+    String END_MESSAGE = "processed text:";
+
     /**
      * Outputs specified message
      *
      * @param message message
      */
     void printMessage(String message);
-
-    void printNumberFormatted(Number message);
 }

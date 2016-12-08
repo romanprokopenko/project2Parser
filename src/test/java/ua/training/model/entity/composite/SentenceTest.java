7eee45b6b7c0f299aea27c5be8275d78a773b31c
Roman Prokopenko <prokopenkor5@gmail.com>
2016-12-08 23:27:47 +0200

    added tests for Sentence

--- src/test/java/ua/training/model/entity/composite/SentenceTest.java
+++ src/test/java/ua/training/model/entity/composite/SentenceTest.java
@@ -4,8 +4,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ua.training.model.entity.ElementOfText;
+import ua.training.model.entity.leaf.PunctuationMark;
+import ua.training.model.entity.leaf.Word;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -15,13 +18,32 @@ import static org.junit.Assert.*;
 public class SentenceTest {
 
     private Sentence sentence;
+    String testString;
 
     @Before
     public void init() {
-        String testString = "Во время наследования возможно изменения" +
+        testString = "Во время наследования возможно изменения" +
                 " модификаторов доступа в сторону большей видимости;.";
         sentence = new Sentence();
         sentence.parse(testString);
+    }
+
+    @Test
+    public void parseWordSizeTest() {
+        int expectedSize = 11;
+        int actualSize = sentence.getSubElements().stream().
+                                                filter(x -> x instanceof Word).
+                                                collect(Collectors.toList()).size();
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void parsePunctuationSizeTest() {
+        int expectedSize = 12;
+        int actualSize = sentence.getSubElements().stream().
+                filter(x -> x instanceof PunctuationMark).
+                collect(Collectors.toList()).size();
+        Assert.assertEquals(expectedSize, actualSize);
     }
 
 }
